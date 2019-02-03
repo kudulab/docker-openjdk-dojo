cc5ac6ae14781ca438f5d08bccb545021986704e
Ewa Czechowska <ewa@ai-traders.com>
2019-02-03 15:53:58 +0100

    do not set docker build ARGs this image name or tag

--- image/etc_dojo.d/variables/60-variables.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-###########################################################################
-# A file to keep any bash variables.
-###########################################################################
-
-export this_image_name="java-ide"
-export this_image_tag="${this_image_tag}"
